5f804b7c76b12d8d160ca2b30a24fdd7c65efcbf
Jorge <jorge.escobar.jimenez@udla.edu.ec>
2020-02-07 18:52:42 -0500

    prj

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('[URL]', '[Controlador@metodo]');
+
+
+Route::get('/', function () {
+    return view('Index');
+
+});
+
+Route::get('/Index', 'Interpretador@Index');
